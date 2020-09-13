77203f15dcd0316b38aa3ea41ac8af930e64b939
khushi choudhary <khushichoudhary12599@gmail.com>
2020-09-13 15:47:48 +0530

    Test

---
+++ Statistics/Stats.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Statistics
+{
+    public class Stats
+    {
+         public double min{get; set;}
+            public double max{get; set;}
+            public double average{get; set;}
+    }
+}
